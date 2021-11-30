0028edbf198217579b2890d10db7a1eccf5d0e7f
CarolinaGomez053 <85083865+danieljaramillo1@users.noreply.github.com>
2021-11-30 08:29:13 -0500

    completed

---
+++ DeleteRenta.php
@@ -0,0 +1,34 @@
+﻿<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+include 'DBConfig.php';
+ 
+$con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+
+$json = file_get_contents('php://input');
+
+$obj = json_decode($json,true);
+ 
+$S_ID = $obj['id'];
+ 
+$Sql_Query = "DELETE FROM renta WHERE id = '$S_ID'" ;
+
+if(mysqli_query($con,$Sql_Query)){
+ 
+$MSG = 'Renta eliminada correctamente...' ;
+ 
+$json = json_encode($MSG);
+ 
+ echo $json ;
+ 
+ }
+ else{
+ 
+ echo 'Inténtelo de nuevo';
+ 
+ }
+ mysqli_close($con);
+?>
---
+++ InsertRenta.php
@@ -0,0 +1,47 @@
+﻿<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+// Importar la conexion
+include 'DBConfig.php';
+ 
+// Conectar a la base de datos
+ $conexion = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ 
+ // Obteniendo los datos en la variable $json.
+ $json = file_get_contents('php://input');
+ 
+ // Decodificando los datos en formato JSON en la variables $obj.
+ $obj = json_decode($json,true);
+ 
+ // Crear variables por cada campo.
+ $S_Id_Book = $obj['libro_id'];
+
+ $S_Id_User = $obj['usuario_id'];
+
+ $S_Renta = $obj['rendate'];
+
+
+ // Instrucción SQL para agregar la renta.
+ $Sql_Query = "insert into renta (libro_id, usuario_id, rendate) values ( '$S_Id_Book', '$S_Id_User','$S_Renta')";
+ 
+ 
+ if(mysqli_query($conexion,$Sql_Query)){
+ 
+$MSG = 'Libro rentado correctamente...' ;
+ 
+//$json = json_encode($MSG);
+//echo $json ;
+ 
+ }
+ else{
+ 
+ $MSG = 'Inténtelo de nuevo...';
+ 
+ }
+ $json = json_encode($MSG);
+ echo $json;
+ mysqli_close($conexion);
+?>
